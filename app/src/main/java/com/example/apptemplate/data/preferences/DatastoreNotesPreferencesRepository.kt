5808cddfbbb45175062aadc518510ac832ad3754
Kostiantyn Levytskyi <v-klevytskyi@expediagroup.com>
2022-09-26 17:57:14 +0300

    refactor data store files

---
+++ app/src/main/java/com/example/apptemplate/data/preferences/DatastoreNotesPreferencesRepository.kt
@@ -0,0 +1,38 @@
+package com.example.apptemplate.data.preferences
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+private const val pinnedNotesKeyName = "pinnedNotes"
+private val pinnedNotesKey = stringSetPreferencesKey(pinnedNotesKeyName)
+
+class DatastoreNotesPreferencesRepository constructor(
+    private val dataStore: DataStore<Preferences>
+): NotesPreferencesRepository {
+
+    override val pinnedNotesIdsFlow: Flow<Set<Int>> = dataStore.data.map { prefs ->
+        prefs[pinnedNotesKey].orEmpty().mapNotNull { it.toIntOrNull() }.toSet()
+    }
+
+    override suspend fun pinNote(noteId: Int) {
+        dataStore.edit { prefs ->
+            prefs[pinnedNotesKey] = prefs[pinnedNotesKey].orEmpty() + noteId.toString()
+        }
+    }
+
+    override suspend fun unpinNote(noteId: Int) {
+        dataStore.edit { prefs ->
+            prefs[pinnedNotesKey] = prefs[pinnedNotesKey].orEmpty() - noteId.toString()
+        }
+    }
+
+    override suspend fun clearAllPins() {
+        dataStore.edit { prefs ->
+            prefs[pinnedNotesKey] = emptySet()
+        }
+    }
+}
--- app/src/main/java/com/example/apptemplate/data/preferences/NotesPreferencesRepository.kt
+++ app/src/main/java/com/example/apptemplate/data/preferences/NotesPreferencesRepository.kt
@@ -1,44 +1,10 @@
 package com.example.apptemplate.data.preferences
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringSetPreferencesKey
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-
-private const val pinnedNotesKeyName = "pinnedNotes"
-private val pinnedNotesKey = stringSetPreferencesKey(pinnedNotesKeyName)
 
 interface NotesPreferencesRepository {
     val pinnedNotesIdsFlow: Flow<Set<Int>>
     suspend fun pinNote(noteId: Int)
     suspend fun unpinNote(noteId: Int)
     suspend fun clearAllPins()
-}
-
-class DatastoreNotesPreferencesRepository constructor(
-    private val dataStore: DataStore<Preferences>
-): NotesPreferencesRepository {
-    override val pinnedNotesIdsFlow: Flow<Set<Int>> = dataStore.data.map { prefs ->
-        prefs[pinnedNotesKey].orEmpty().mapNotNull { it.toIntOrNull() }.toSet()
-    }
-
-    override suspend fun pinNote(noteId: Int) {
-        dataStore.edit { prefs ->
-            prefs[pinnedNotesKey] = prefs[pinnedNotesKey].orEmpty() + noteId.toString()
-        }
-    }
-
-    override suspend fun unpinNote(noteId: Int) {
-        dataStore.edit { prefs ->
-            prefs[pinnedNotesKey] = prefs[pinnedNotesKey].orEmpty() - noteId.toString()
-        }
-    }
-
-    override suspend fun clearAllPins() {
-        dataStore.edit { prefs ->
-            prefs[pinnedNotesKey] = emptySet()
-        }
-    }
 }
