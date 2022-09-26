12bd4c126b9a0f2897ef862624b2e75e80b18809
Kostiantyn Levytskyi <v-klevytskyi@expediagroup.com>
2022-09-26 20:13:21 +0300

    refactor all notes screen

--- app/src/main/java/com/example/apptemplate/ui/allNotes/AllNotesScreen.kt
+++ app/src/main/java/com/example/apptemplate/ui/allNotes/AllNotesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +18,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetScaffoldState
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
@@ -41,6 +43,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -59,50 +62,28 @@ fun AllNotesScreen(
     viewModel: AllNotesViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiStateFlow.collectAsStateWithLifecycle()
-
     val scaffoldState = rememberBottomSheetScaffoldState()
-
+    val scope = rememberCoroutineScope()
     var isDeleteAllNotesDialogShown by remember { mutableStateOf(false) }
 
-    if (isDeleteAllNotesDialogShown) {
-        AlertDialog(
-            onDismissRequest = { isDeleteAllNotesDialogShown = false },
-            text = {
-                Text(text = "Do you want to delete all notes?")
-            },
-            confirmButton = {
-                Button(onClick = {
-                    viewModel.deleteAllNotes()
-                    isDeleteAllNotesDialogShown = false
-                }) {
-                    Text(text = "Delete")
-                }
-            },
-            dismissButton = {
-                Button(onClick = {
-                    isDeleteAllNotesDialogShown = false
-                }) {
-                    Text(text = "Cancel")
-                }
-            }
-        )
-    }
-
-    val scope = rememberCoroutineScope()
-
-    LaunchedEffect(uiState.focusedNote) {
-        if (uiState.focusedNote != null) {
-            scaffoldState.bottomSheetState.expand()
-        }
-    }
-
-    LaunchedEffect(scaffoldState.bottomSheetState.isCollapsed) {
-        if (scaffoldState.bottomSheetState.isCollapsed) {
-            viewModel.onNoteFocused(null)
-        }
-    }
-
     BottomSheetScaffold(
+        topBar = {
+            AllNotesAppBar(
+                onDeleteAllNotesClicked = { isDeleteAllNotesDialogShown = true },
+                uiState = uiState
+            )
+        },
+        content = { paddingValues ->
+            AllNotesContent(paddingValues, uiState, onOpenNote, viewModel::onNoteFocused)
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onCreateNewNote) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        },
+        sheetContent = {
+            AllNotesBottomSheetContent(uiState, viewModel, scaffoldState)
+        },
         modifier = Modifier
             .background(MaterialTheme.colors.background)
             .fillMaxSize()
@@ -114,166 +95,119 @@ fun AllNotesScreen(
                     scaffoldState.bottomSheetState.collapse()
                 }
             },
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Notes")
-                },
-                actions = {
-                    if (uiState.pinnedNotes.isNotEmpty() || uiState.notPinnedNotes.isNotEmpty()) {
-                        Icon(
-                            imageVector = Icons.Default.Delete,
-                            contentDescription = null,
-                            modifier = Modifier.clickable {
-                                isDeleteAllNotesDialogShown = true
-                            })
-                    }
-                }
-            )
-        },
-        content = { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-            ) {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 8.dp)
-                ) {
-                    if (uiState.pinnedNotes.isNotEmpty()) {
-                        item {
-                            Text(
-                                text = "Pinned",
-                                style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
-                                modifier = Modifier.padding(
-                                    top = 12.dp,
-                                    bottom = 12.dp,
-                                    start = 8.dp
-                                )
-                            )
-                        }
-
-                        itemsIndexed(
-                            uiState.pinnedNotes,
-                            key = { _: Int, item: PinnableNote -> item.note.id }
-                        ) { index, note ->
-                            val shape = when {
-                                uiState.pinnedNotes.size == 1 -> NoteItemPosition.SINGLE_ITEM
-                                index == 0 -> NoteItemPosition.START_OF_LIST
-                                index == uiState.pinnedNotes.lastIndex -> NoteItemPosition.END_OF_LIST
-                                else -> NoteItemPosition.MIDDLE_OF_LIST
-                            }
-                            NoteItem(
-                                position = shape,
-                                pinnableNote = note,
-                                onNoteClicked = onOpenNote,
-                                onLongPressed = viewModel::onNoteFocused
-                            )
-                        }
-                    }
-
-                    item {
-                        Text(
-                            text = "Notes",
-                            style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
-                            modifier = Modifier.padding(top = 12.dp, bottom = 12.dp, start = 8.dp)
-                        )
-                    }
-
-                    itemsIndexed(
-                        uiState.notPinnedNotes,
-                        key = { _: Int, item: PinnableNote -> item.note.id }
-                    ) { index, note ->
-                        val shape = when {
-                            uiState.notPinnedNotes.size == 1 -> NoteItemPosition.SINGLE_ITEM
-                            index == 0 -> NoteItemPosition.START_OF_LIST
-                            index == uiState.notPinnedNotes.lastIndex -> NoteItemPosition.END_OF_LIST
-                            else -> NoteItemPosition.MIDDLE_OF_LIST
-                        }
-                        NoteItem(
-                            position = shape,
-                            pinnableNote = note,
-                            onNoteClicked = onOpenNote,
-                            onLongPressed = viewModel::onNoteFocused
-                        )
-                    }
-                }
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = onCreateNewNote) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = null)
-            }
-        },
         scaffoldState = scaffoldState,
-        sheetPeekHeight = 0.dp,
-        sheetContent = {
-            NoteActionContent(
-                pinnableNote = uiState.focusedNote,
-                onChangePinClicked = {
-                    uiState.focusedNote?.run {
-                        if (isPinned) {
-                            viewModel.unpinNote(note)
-                        } else {
-                            viewModel.pinNote(note)
-                        }
-                        scope.launch {
-                            scaffoldState.bottomSheetState.collapse()
-                        }
-                    }
-                },
-                onDeleteNoteClicked = {
-                    uiState.focusedNote?.let {
-                        viewModel.deleteNote(it.note)
-                        scope.launch {
-                            scaffoldState.bottomSheetState.collapse()
-                        }
-                    }
+        sheetPeekHeight = 0.dp
+    )
+
+    OpenBottomSheetOnNoteFocused(uiState, scaffoldState)
+
+    ClearFocusedNoteOnBottomSheetCollapsed(scaffoldState, viewModel)
+
+    if (isDeleteAllNotesDialogShown) {
+        AlertDialog(
+            onDismissRequest = { isDeleteAllNotesDialogShown = false },
+            text = {
+                Text(text = stringResource(R.string.delete_all_notes_confirmation))
+            },
+            confirmButton = {
+                Button(onClick = {
+                    viewModel.deleteAllNotes()
+                    isDeleteAllNotesDialogShown = false
+                }) {
+                    Text(text = stringResource(R.string.delete))
                 }
-            )
+            },
+            dismissButton = {
+                Button(onClick = {
+                    isDeleteAllNotesDialogShown = false
+                }) {
+                    Text(text = stringResource(R.string.cancel))
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun AllNotesAppBar(
+    onDeleteAllNotesClicked: () -> Unit,
+    uiState: AllNotesUiState
+) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(R.string.notes))
+        },
+        actions = {
+            if (uiState.pinnedNotes.isNotEmpty() || uiState.notPinnedNotes.isNotEmpty()) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    modifier = Modifier.clickable(onClick = onDeleteAllNotesClicked)
+                )
+            }
         }
     )
 }
 
 @Composable
-fun NoteActionContent(
-    pinnableNote: PinnableNote?,
-    onChangePinClicked: () -> Unit,
-    onDeleteNoteClicked: () -> Unit
+private fun AllNotesContent(
+    paddingValues: PaddingValues,
+    uiState: AllNotesUiState,
+    onOpenNote: (NoteEntity) -> Unit,
+    onNoteFocused: (PinnableNote) -> Unit
 ) {
-    Box(
+    Column(
         modifier = Modifier
-            .height(64.dp)
-            .fillMaxWidth()
-            .padding(start = 8.dp)
-            .clickable(onClick = onChangePinClicked),
-        contentAlignment = Alignment.CenterStart
+            .fillMaxSize()
+            .padding(paddingValues)
     ) {
-        val text = if (pinnableNote?.isPinned == true) "Unpin" else "Pin"
-        Text(text = text, style = MaterialTheme.typography.body1)
-    }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp)
+        ) {
+            if (uiState.pinnedNotes.isNotEmpty()) {
+                item {
+                    NotesSectionTitle(text = stringResource(R.string.pinned_notes))
+                }
 
-    Box(
-        modifier = Modifier
-            .height(64.dp)
-            .fillMaxWidth()
-            .padding(start = 8.dp)
-            .clickable(onClick = onDeleteNoteClicked),
-        contentAlignment = Alignment.CenterStart
-    ) {
-        Text(text = "Delete", style = MaterialTheme.typography.body1)
-    }
-}
+                itemsIndexed(
+                    uiState.pinnedNotes,
+                    key = { _: Int, item: PinnableNote -> item.note.id }
+                ) { index, note ->
+                    val noteItemPosition = getNoteItemPosition(uiState.pinnedNotes, index)
+                    NoteItem(
+                        position = noteItemPosition,
+                        pinnableNote = note,
+                        onNoteClicked = onOpenNote,
+                        onLongPressed = onNoteFocused
+                    )
+                }
+            }
 
-enum class NoteItemPosition {
-    START_OF_LIST, MIDDLE_OF_LIST, END_OF_LIST, SINGLE_ITEM
+            item {
+                NotesSectionTitle(text = stringResource(R.string.notes))
+            }
+
+            itemsIndexed(
+                uiState.notPinnedNotes,
+                key = { _: Int, item: PinnableNote -> item.note.id }
+            ) { index, note ->
+                val noteItemPosition = getNoteItemPosition(uiState.notPinnedNotes, index)
+                NoteItem(
+                    position = noteItemPosition,
+                    pinnableNote = note,
+                    onNoteClicked = onOpenNote,
+                    onLongPressed = onNoteFocused
+                )
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NoteItem(
+private fun NoteItem(
     position: NoteItemPosition,
     pinnableNote: PinnableNote,
     onNoteClicked: (NoteEntity) -> Unit,
@@ -294,11 +228,13 @@ fun NoteItem(
     ) {
         Column(
             Modifier
-                .combinedClickable(onClick = {
-                    onNoteClicked(pinnableNote.note)
-                }, onLongClick = {
-                    onLongPressed(pinnableNote)
-                })
+                .combinedClickable(
+                    onClick = {
+                        onNoteClicked(pinnableNote.note)
+                    }, onLongClick = {
+                        onLongPressed(pinnableNote)
+                    }
+                )
         ) {
             Column(modifier = Modifier.padding(8.dp)) {
                 Text(
@@ -322,4 +258,128 @@ fun NoteItem(
             }
         }
     }
+}
+
+@Composable
+private fun NotesSectionTitle(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
+        modifier = Modifier.padding(
+            top = 12.dp,
+            bottom = 12.dp,
+            start = 8.dp
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun AllNotesBottomSheetContent(
+    uiState: AllNotesUiState,
+    viewModel: AllNotesViewModel,
+    scaffoldState: BottomSheetScaffoldState
+) {
+    val scope = rememberCoroutineScope()
+
+    NoteActionContent(
+        pinnableNote = uiState.focusedNote,
+        onChangePinClicked = {
+            uiState.focusedNote?.run {
+                if (isPinned) {
+                    viewModel.unpinNote(note)
+                } else {
+                    viewModel.pinNote(note)
+                }
+                scope.launch {
+                    scaffoldState.bottomSheetState.collapse()
+                }
+            }
+        },
+        onDeleteNoteClicked = {
+            uiState.focusedNote?.let {
+                viewModel.deleteNote(it.note)
+                scope.launch {
+                    scaffoldState.bottomSheetState.collapse()
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun NoteActionContent(
+    pinnableNote: PinnableNote?,
+    onChangePinClicked: () -> Unit,
+    onDeleteNoteClicked: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .height(64.dp)
+            .fillMaxWidth()
+            .clickable(onClick = onChangePinClicked),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        val textId = if (pinnableNote?.isPinned == true) R.string.unpin_note else R.string.pin_note
+        Text(
+            text = stringResource(textId),
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+
+    Box(
+        modifier = Modifier
+            .height(64.dp)
+            .fillMaxWidth()
+            .clickable(onClick = onDeleteNoteClicked),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(
+            text = stringResource(R.string.delete),
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun ClearFocusedNoteOnBottomSheetCollapsed(
+    scaffoldState: BottomSheetScaffoldState,
+    viewModel: AllNotesViewModel
+) {
+    LaunchedEffect(scaffoldState.bottomSheetState.isCollapsed) {
+        if (scaffoldState.bottomSheetState.isCollapsed) {
+            viewModel.onNoteFocused(null)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun OpenBottomSheetOnNoteFocused(
+    uiState: AllNotesUiState,
+    scaffoldState: BottomSheetScaffoldState
+) {
+    LaunchedEffect(uiState.focusedNote) {
+        if (uiState.focusedNote != null) {
+            scaffoldState.bottomSheetState.expand()
+        }
+    }
+}
+
+private enum class NoteItemPosition {
+    START_OF_LIST, MIDDLE_OF_LIST, END_OF_LIST, SINGLE_ITEM
+}
+
+@Composable
+private fun getNoteItemPosition(
+    notesList: List<PinnableNote>,
+    index: Int
+) = when {
+    notesList.size == 1 -> NoteItemPosition.SINGLE_ITEM
+    index == 0 -> NoteItemPosition.START_OF_LIST
+    index == notesList.lastIndex -> NoteItemPosition.END_OF_LIST
+    else -> NoteItemPosition.MIDDLE_OF_LIST
 }
